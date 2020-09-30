0170fe5012c522eea4ffc5a78ce160ff4050108a
Vladislav Olhovik <alkhovikvlad@gmail.com>
2020-09-30 21:21:39 +0200

    Updates

---
+++ precache-manifest.f5ed5daecc4fd5a14f878549338c1b0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c71ad793be83694c1fa04a45de61e2e",
+    "url": "/SocialNetwork-TS/index.html"
+  },
+  {
+    "revision": "6078d5ff5e2ce6fdbd19",
+    "url": "/SocialNetwork-TS/static/css/main.d9e4f68c.chunk.css"
+  },
+  {
+    "revision": "39c515faa78e4f1e4e7c",
+    "url": "/SocialNetwork-TS/static/js/2.d308a6bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocialNetwork-TS/static/js/2.d308a6bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6078d5ff5e2ce6fdbd19",
+    "url": "/SocialNetwork-TS/static/js/main.07e504d2.chunk.js"
+  },
+  {
+    "revision": "c6339b0b6cbe394f768b",
+    "url": "/SocialNetwork-TS/static/js/runtime-main.a7beff3c.js"
+  }
+]);
