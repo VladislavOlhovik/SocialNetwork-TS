208b1b051bc8586c96a8db745445f5f07873fe14
Vladislav Olhovik <alkhovikvlad@gmail.com>
2020-11-18 20:40:29 +0100

    redux-form lesson 75

--- src/Redux/redux-store.ts
+++ src/Redux/redux-store.ts
@@ -5,6 +5,8 @@ import dialogsReducer from './dialogs-reducer';
 import profileReducer from './profile-reducer';
 import sidebarReducer from './sidebar-reducer';
 import usersReducer from './users-reducer';
+import { reducer as formReducer } from 'redux-form'
+
 
 let reducers = combineReducers({
     dialogsPage:dialogsReducer,
@@ -12,6 +14,7 @@ let reducers = combineReducers({
     sidebar:sidebarReducer,
     usersPage:usersReducer,
     auth:authReducer,
+    form: formReducer,
 })
 
 export type RootType = ReturnType<typeof reducers>
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,7 +1,43 @@
 import React from 'react'
+import { Field, InjectedFormProps, reduxForm } from 'redux-form'
+
+const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={'Login'} name={'login'} component={'input'} />
+            </div>
+            <div>
+                <Field placeholder={'Password'} name={'password'} component={'input'}/>
+            </div>
+            <div>
+                <Field type={'checkbox'} name={'rememberMe'} component={'input'}/>remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+const LogiReduxForm = reduxForm<FormDataType>({form: 'login'})(LoginForm)
+
+type FormDataType = {
+  login: string
+  password: string
+  rememberMe: boolean
+};
 
 const Login = () => {
-    return <h4>Login</h4>
+    const onSubmit = (formData:FormDataType) => {
+        console.log(formData);
+    }
+    return (
+      <div>
+        <h1>Login</h1>
+        <LogiReduxForm onSubmit={onSubmit}/>
+      </div>
+    );
 }
 
 export default Login
